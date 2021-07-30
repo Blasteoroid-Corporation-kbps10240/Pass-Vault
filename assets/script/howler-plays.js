8506568e694c5513c10a78ce0f409698df5b0fcc
Blasteoroid Corporation <67875263+Blasteoroid-Corporation-kbps10240@users.noreply.github.com>
2021-07-30 10:15:57 +0530

    Delete assets/script directory

--- assets/script/howler-plays.js
+++
@@ -1,9 +0,0 @@
-var sound = new Howl({
-    src: ['../assets/audio/MenuMusic01.mp3'],
-    volume: 1.0,
-    onend: function () {
-      //alert('We finished with the setup!');
-      sound.play()
-    }
-  });
-  sound.play()
