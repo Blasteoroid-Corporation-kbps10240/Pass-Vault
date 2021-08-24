2cde6ce0d8cd8267df1656de625e5acbbbffbd49
Blasteoroid Corporation <67875263+Blasteoroid-Corporation-kbps10240@users.noreply.github.com>
2021-08-25 01:04:53 +0530

    Add files via upload

---
+++ secondary/infok/assets/js/howler-plays.js
@@ -0,0 +1,36 @@
+/*var audio = new Howl({
+    src: ['../assets/audio/MenuMusic01.mp3'],
+    volume: 1.0,
+
+    onend: function () {
+      //alert('We finished with the setup!');
+      audio.play()
+    },
+
+    onpause: function(){
+        audio.play()
+        
+    }
+  });
+  audio.play()
+*/
+
+
+
+// where to find flash SWFs, if needed...
+/*soundManager.url = '/path/to/swf-files/';
+
+soundManager.onready(function() {
+    soundManager.createSound({
+            id: 'mySound',
+            url: '../assets/audio/MenuMusic01.mp3'
+        });
+
+        // ...and play it
+        soundManager.play('mySound');
+});
+*/
+
+/*window.onload=function(){
+  document.getElementById("my_audio").play();
+}*/
